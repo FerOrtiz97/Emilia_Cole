ab7ae0aee4346e4fba836a537c0cffee40439910
FerOrtiz97 <tino.pipe97@gmail.com>
2023-10-22 23:32:25 -0300

    agregue los estilos a la pagina

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 3000
 
-app.listen(3000, () => {
-    console.log('Servidor levantado en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Servidor levantado en http://localhost:${3000}`);
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views/index.html'))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views/contact.html'))
+    res.sendFile(path.join(__dirname, '/views/contact.html'))
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views/about.html'))
+    res.sendFile(path.join(__dirname, '/views/about.html'))
 })
 
 app.get('/music', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views/music.html'))
+    res.sendFile(path.join(__dirname, '/views/music.html'))
 })
 
 app.use(express.static(__dirname + '/public'));
